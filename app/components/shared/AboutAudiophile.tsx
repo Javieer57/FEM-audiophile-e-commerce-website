fa3e8a397749722c2f24b0a825a8b54a760bde45
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2026-05-26 12:56:48 -0600

    fix: home sections responsive styles

--- app/components/shared/AboutAudiophile.tsx
+++ app/components/shared/AboutAudiophile.tsx
@@ -1,8 +1,8 @@
 export function AboutAudiophile() {
   return (
     <section aria-labelledby="about-title" className="general-container">
-      <div className="grid items-center gap-10 md:gap-16 lg:grid-cols-2 lg:gap-32">
-        <div className="space-y-8 max-lg:order-2 max-lg:text-center md:max-lg:mx-auto md:max-lg:max-w-11/12">
+      <div className="grid items-center gap-10 md:gap-16 lg:grid-cols-12 lg:gap-7.5">
+        <div className="space-y-8 max-lg:order-2 max-lg:text-center md:max-lg:mx-auto md:max-lg:max-w-11/12 lg:col-span-5">
           <h2
             id="about-title"
             className="text-[1.75rem] font-bold tracking-[0.06rem] uppercase md:text-[2.5rem] md:leading-11 md:tracking-[0.09rem]"
@@ -10,7 +10,7 @@ export function AboutAudiophile() {
             BRINGING YOU THE <span className="text-primary">BEST</span> AUDIO
             GEAR
           </h2>
-          <p className="font-medium text-balance opacity-50">
+          <p className="font-medium opacity-50 max-lg:text-balance">
             Located at the heart of New York City, Audiophile is the premier
             store for high end headphones, earphones, speakers, and audio
             accessories. We have a large showroom and luxury demonstration rooms
@@ -20,7 +20,9 @@ export function AboutAudiophile() {
           </p>
         </div>
 
-        <div>
+        <div className="hidden lg:block"></div>
+
+        <div className="lg:col-span-6">
           <picture>
             <source
               media="(min-width: 1024px)"
